61d655749dfadb78e78ca845f09fadd0c150d631
metalnow <shaune.jan@gmail.com>
2014-05-26 17:15:02 +0800

    recover default value
    recover default PID values from old firmware

--- Code/EEPROM.ino
+++ Code/EEPROM.ino
@@ -144,16 +144,16 @@ void update_constants() {
 void LoadDefaults() {
   uint8_t i;
   #ifndef SUPPRESS_DEFAULTS_FROM_GUI
-    conf.pid[ROLL].P8     = 33;  conf.pid[ROLL].I8    = 30; conf.pid[ROLL].D8     = 23;
-    conf.pid[PITCH].P8    = 33; conf.pid[PITCH].I8    = 30; conf.pid[PITCH].D8    = 23;
-    conf.pid[YAW].P8      = 68;  conf.pid[YAW].I8     = 45;  conf.pid[YAW].D8     = 0;
-    conf.pid[PIDALT].P8   = 64; conf.pid[PIDALT].I8   = 25; conf.pid[PIDALT].D8   = 24;
+    conf.pid[ROLL].P8     = 40;  conf.pid[ROLL].I8    = 30; conf.pid[ROLL].D8     = 23;
+    conf.pid[PITCH].P8    = 40; conf.pid[PITCH].I8    = 30; conf.pid[PITCH].D8    = 23;
+    conf.pid[YAW].P8      = 85;  conf.pid[YAW].I8     = 45;  conf.pid[YAW].D8     = 0;
+    conf.pid[PIDALT].P8   = 50; conf.pid[PIDALT].I8   = 20; conf.pid[PIDALT].D8   = 30;
 
     conf.pid[PIDPOS].P8  = POSHOLD_P * 100;     conf.pid[PIDPOS].I8    = POSHOLD_I * 100;       conf.pid[PIDPOS].D8    = 0;
     conf.pid[PIDPOSR].P8 = POSHOLD_RATE_P * 10; conf.pid[PIDPOSR].I8   = POSHOLD_RATE_I * 100;  conf.pid[PIDPOSR].D8   = POSHOLD_RATE_D * 1000;
     conf.pid[PIDNAVR].P8 = NAV_P * 10;          conf.pid[PIDNAVR].I8   = NAV_I * 100;           conf.pid[PIDNAVR].D8   = NAV_D * 1000;
   
-    conf.pid[PIDLEVEL].P8 = 90; conf.pid[PIDLEVEL].I8 = 10; conf.pid[PIDLEVEL].D8 = 100;
+    conf.pid[PIDLEVEL].P8 = 70; conf.pid[PIDLEVEL].I8 = 10; conf.pid[PIDLEVEL].D8 = 100;
     conf.pid[PIDMAG].P8   = 40;
 
     conf.pid[PIDVEL].P8 = 0;      conf.pid[PIDVEL].I8 = 0;    conf.pid[PIDVEL].D8 = 0;
